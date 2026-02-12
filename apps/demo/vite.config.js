b1eefd59fa31f2c56949c187278c91bc419ae931
cyper8 <anton.yashchuk@gmail.com>
2026-02-12 13:42:24 +0200

    app-demo works

--- apps/demo/vite.config.js
+++ apps/demo/vite.config.js
@@ -1,20 +1,9 @@
 /** @type {import('vite').UserConfig} */
 
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
 
 export default defineConfig({
-  base: './',
   server: {
-    base: "./",
-    cors: false,
-    fs: {
-      allow: ['..','../..']
-    }
-  },
-  resolve: {
-    alias: {
-      '@bible-reading': '../../packages/bible-reading/index.js'
-    }
+    cors: false
   }
 })
--- packages/bible-reading-calendar/src/bible-reading-calendar.ts
+++ packages/bible-reading-calendar/src/bible-reading-calendar.ts
@@ -1,4 +1,4 @@
-import { LitElement, PropertyValueMap, css, html } from "lit";
+import { LitElement, PropertyValueMap, TemplateResult, css, html } from "lit";
 import { customElement, property, state } from "lit/decorators.js";
 import { classMap } from "lit/directives/class-map.js";
 import { styleMap } from "lit/directives/style-map.js";
